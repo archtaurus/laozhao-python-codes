0be1cc584b7fe47fccf9d151658e45802fe984ba
archtaurus <7176466@qq.com>
2016-11-11 08:26:03 +0800

    new

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# laozhao-python-codes
-老赵的Python代码碎片
+# 老赵的Python代码碎片
+
+目录：
+- pycode0x0000 : Hello, Python!
+- pycode0x0001 : 数字1、2、3、4能组成多少个无重复数字的三位数？
---
+++ pycode0x0001.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding:utf-8 -*-
+
+# 数字1、2、3、4能组成多少个无重复数字的三位数？
+
+numbers = [100 * i + 10 * j + k
+           for i in [1, 2, 3, 4] for j in [1, 2, 3, 4] for k in [1, 2, 3, 4]
+           if i != j and j != k and k != i]
+print "Total:", len(numbers)
+for number in numbers:
+    print (number)
