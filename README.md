bc72984bf39dd6fa958178c5cf7a8e7b2a9ebb3a
archtaurus <7176466@qq.com>
2016-11-11 09:46:15 +0800

    new

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 目录：
 - pycode0x0000 : Hello, Python!
 - pycode0x0001 : 数字1、2、3、4能组成多少个无重复数字的三位数？
+- pycode0x0002 : 斐波拉契数列生成器
---
+++ pycode0x0002.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+# -*- coding:utf-8 -*-
+#
+# filename: fib.py
+# version: 2016.04.04
+# author: Zhao Xin (赵鑫)
+# email: pythonchallenge@qq.com
+#
+# 斐波那契数列（Fibonacci sequence）又称黄金分割数列，因数学家列昂纳多·斐波那契
+# 以兔子繁殖为例子而引入，故又称为“兔子数列”，指的是这样一个数列：
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...
+# 在数学上斐波纳契数列以如下递归的方法定义：
+# F(0) = 0, F(1) = 1, F(n) = F(n-1) + F(n-2) (n≥2, n∈N*)
+# 在现代物理、准晶体结构、化学等领域，斐波纳契数列都有直接的应用。
+
+
+def fib(n):
+    """长度为n的斐波拉契数列生成器"""
+    a, b = 0, 1
+    for _ in xrange(n):
+        yield a
+        a, b = a + b, a
+
+
+if __name__ == "__main__":
+    for i in fib(10):
+        print i,
