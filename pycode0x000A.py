730fce8dca9eeb455f4becfefdf2423786db2bf0
archtaurus <7176466@qq.com>
2016-11-11 12:16:01 +0800

    新建

--- pycode0x000A.py
+++ pycode0x000A.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python2
 # -*- coding:utf-8 -*-
 #
-# 已知10个半径，求圆面积的和，要求保留3位小数(派和四舍五入)
+# 求出两个三位数的乘积中最大的回文数(分片、max、生成器推导式)
 
-from math import pi
 
-radius = [45, 58, 21, 61, 42, 69, 49, 88, 83, 27]
-area = round(sum(pi * r**2 for r in radius), 3)  # 四舍五入
+def is_palnum(n):
+    "判断十进制数n是否是回文数(palindromic number)"
+    # 简洁的，但低效的方式: return str(n) == str(n)[::-1]
+    sn = str(n)         # string n
+    hl = len(sn) / 2    # half of length
+    left, right = sn[:hl], sn[hl:][::-1]  # half of string (both L2R and R2L)
+    return all(left[i] == right[i] for i in xrange(hl))
 
-print area
+
+if __name__ == '__main__':
+    print max(x * y
+              for x in xrange(100, 1000)
+              for y in xrange(100, 1000)
+              if is_palnum(x * y))
